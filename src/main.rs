10d2100bb197dd64aabe77734b3c0c36687d32f1
juanloaiza21 <juan400reyesloaiza@gmail.com>
2025-07-12 13:51:33 -0500

    Modularizacion

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
-use axum::{Router, routing::get, serve::Listener};
+use axum::{Router, response::IntoResponse, response::IntoResponseParts, routing::get};
+
+async fn hello_world() -> impl IntoResponse {
+    "hello_world!"
+}
+
+fn init_router() -> Router {
+    Router::new().route("/", get(hello_world))
+}
 
 #[tokio::main]
 async fn main() {
-    //Build the app with just one route
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    axum::serve(listener, app).await.unwrap();
+    let app = init_router();
+    let listner = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listner, app).await.unwrap();
 }
